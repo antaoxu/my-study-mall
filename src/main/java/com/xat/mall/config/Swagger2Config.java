471bb91e4f33f8dc224fa2c171404994ce7ee4a2
antaoxu97@163.com <antaoxu97@163.com>
2021-09-03 17:09:42 +0800

    引入Swagger接口文档

---
+++ src/main/java/com/xat/mall/config/Swagger2Config.java
@@ -0,0 +1,50 @@
+package com.xat.mall.config;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author xuantao
+ * @Swagger2 API接口文档的配置
+ *
+ * 生成指定包下面的类的API文档
+ * 生成有指定注解的类的API文档
+ * 生成有指定注解的方法的API文档
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("com.xat.mall.controller"))
+                //为有@Api注解的Controller生成API文档
+                //.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+                //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("SwaggerUI演示")
+                .description("mall")
+                .contact("xat")
+                .version("1.0")
+                .build();
+    }
+
+}
--- src/main/java/com/xat/mall/controller/PmsBrandController.java
+++ src/main/java/com/xat/mall/controller/PmsBrandController.java
@@ -3,10 +3,13 @@ package com.xat.mall.controller;
 import com.xat.mall.common.api.CommonResult;
 import com.xat.mall.mbg.model.PmsBrand;
 import com.xat.mall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,19 +19,37 @@ import java.util.List;
 /**
  * @author 淡漠
  */
+@Api(tags = "PmsBrandController",value = "商品品牌管理")
 @Controller
 @RequestMapping("/brand")
 public class PmsBrandController {
+
     @Autowired
     private PmsBrandService pmsBrandService;
 
     private static final Logger logger = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @ApiOperation("获取所有品牌")
     @RequestMapping(value = "listAll",method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> getBrandList() {
         return CommonResult.success(pmsBrandService.listAllBrands());
     }
 
+    @ApiOperation("创建品牌")
+    @RequestMapping(value = "/createBrand",method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<Integer> createBrand(@RequestBody PmsBrand pmsBrand){
+        CommonResult commonResult;
+        int count = pmsBrandService.createBrand(pmsBrand);
+        if (count == 1){
+            commonResult = CommonResult.success(count);
+            logger.debug("createBrand success:{}",pmsBrand);
+        }else {
+            commonResult = CommonResult.failed("操作失败");
+            logger.debug("createBrand failed:{}",pmsBrand);
+        }
+        return commonResult;
+    }
 
 }
